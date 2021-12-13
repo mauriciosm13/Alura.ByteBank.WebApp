4d8f110d6b09af4d8863ed1dbcc629a29232ca53
André Bessa <andrebessax@gmail.com>
2021-12-13 16:22:52 -0300

    Update: Ajustes aula 04.

--- Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
+++ Alura.ByteBank.WebApp.Testes/NavegandoNaPaginaHome.cs
@@ -10,14 +10,11 @@ namespace Alura.ByteBank.WebApp.Testes
 {
     public class NavegandoNaPaginaHome:IClassFixture<Fixture>
     {
-        //private readonly string diretorio;
-        private IWebDriver driver;
+         private IWebDriver driver;
 
         //Setup
         public NavegandoNaPaginaHome(Fixture fixture)
         {
-            //diretorio = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            //driver = new ChromeDriver(diretorio);
             driver = fixture.Driver;
         }
         [Fact]
@@ -42,14 +39,7 @@ namespace Alura.ByteBank.WebApp.Testes
             //Assert
             Assert.Contains("Login", driver.PageSource);
             Assert.Contains("Home", driver.PageSource);
-
         }
 
-        ////Cleanup
-        //public void Dispose()
-        //{
-        //    //Fechar o navegador
-        //    driver.Quit();
-        //}
     }
 }
